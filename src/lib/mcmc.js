0a962033e99d88f15a1ad31d0480c44ef02211a2
SIKX HILTON <sikxhilton@gmail.com>
2026-01-21 02:54:38 -0800

    Create mcmc.js

---
+++ src/lib/mcmc.js
@@ -0,0 +1,25 @@
+/* code/src/workers/mcmcWorker.js
+ * Web Worker: runs ensemble MCMC off UI thread and posts progress.
+ */
+
+import { runEnsembleMCMC } from "../lib/mcmc.js";
+
+self.onmessage = (e) => {
+  const msg = e.data;
+  if (!msg || msg.type !== "RUN") return;
+
+  try {
+    const { ptaData, options } = msg;
+
+    const result = runEnsembleMCMC(ptaData, options, (progress) => {
+      self.postMessage({ type: "PROGRESS", progress });
+    });
+
+    self.postMessage({ type: "DONE", result });
+  } catch (err) {
+    self.postMessage({
+      type: "ERROR",
+      message: err?.message ?? String(err),
+    });
+  }
+};
