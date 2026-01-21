f84036c28e7380f0a35202807be93c06260c46c1
SIKX HILTON <sikxhilton@gmail.com>
2026-01-21 01:49:35 -0800

    Create App.jsx

---
+++ src/src/src/lib/src/lib/src/lib/src/lib/src/workers/src/components/src/App.jsx
@@ -0,0 +1,8 @@
+/* code/src/App.jsx */
+
+import React from "react";
+import CosmicSuperstringAnalysis from "./components/CosmicSuperstringAnalysis.jsx";
+
+export default function App() {
+  return <CosmicSuperstringAnalysis />;
+}
